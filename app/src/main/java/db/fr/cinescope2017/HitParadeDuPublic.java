57c16cb324acd2702f937f71a031a54588434a06
Destin <bak1tino@gmail.com>
2017-10-31 15:09:45 +0100

    Mise en place de la fenêtre Hit parade du public

--- app/src/main/java/db/fr/cinescope2017/HitParadeDuPublic.java
+++ app/src/main/java/db/fr/cinescope2017/HitParadeDuPublic.java
@@ -4,10 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import asyncpackage.TacheAsynchrone;
 
 public class HitParadeDuPublic extends AppCompatActivity implements View.OnClickListener{
     private Button buttonValider;
     private Button buttonAnnuler;
+    private TextView textViewContenu;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,8 +21,18 @@ public class HitParadeDuPublic extends AppCompatActivity implements View.OnClick
         buttonValider = (Button)findViewById(R.id.buttonValider);
         buttonAnnuler = (Button)findViewById(R.id.buttonAnnuler);
 
+        textViewContenu = (TextView) findViewById(R.id.textViewContenu);
+
         buttonValider.setOnClickListener(this);
         buttonAnnuler.setOnClickListener(this);
+
+        String lsURL = "http://172.26.10.3:8084/Cinescope2017Web/";
+        String lsRessource = "HitParadeDuPublic";
+        //new TacheAsynchrone().execute(lsURL, lsRessource);
+        TacheAsynchrone tae = new TacheAsynchrone();
+        tae.setTextViewCSV(textViewContenu);
+        tae.execute(lsURL, lsRessource);
+
     }
 
     @Override
