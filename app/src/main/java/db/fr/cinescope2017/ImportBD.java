041acbe5e7fcb20cfb17ff850cc59704da9d1b02
Destin <bak1tino@gmail.com>
2017-11-07 16:13:09 +0100

    Mise en place du Content Provider: fournisseur de données à d'autres applications

--- app/src/main/java/db/fr/cinescope2017/ImportBD.java
+++ app/src/main/java/db/fr/cinescope2017/ImportBD.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import org.json.JSONArray;
@@ -20,8 +21,9 @@ import asyncpackage.GenericAsyncTask;
 import utilities.db.OpenManagerSQLite;
 
 public class ImportBD extends AppCompatActivity implements View.OnClickListener {
-//    private OpenManagerSQLite oms;
+    //    private OpenManagerSQLite oms;
 //    private SQLiteDatabase bd;
+    private ProgressBar barreDeProgression;
 
     private TextView textViewMessage;
     private Button countriesImportButton;
@@ -39,6 +41,7 @@ public class ImportBD extends AppCompatActivity implements View.OnClickListener 
      *
      */
     private void initInterface() {
+        barreDeProgression = (ProgressBar) findViewById(R.id.barreDeProgression);
         textViewMessage = (TextView) findViewById(R.id.textViewMessage);
         countriesImportButton = (Button) findViewById(R.id.countriesImportButton);
     }
@@ -57,6 +60,9 @@ public class ImportBD extends AppCompatActivity implements View.OnClickListener 
     public void onClick(View v) {
 
         if (v == countriesImportButton) {
+            barreDeProgression.setProgress(0);
+            this.textViewMessage.setText(0 + "%");
+
             OpenManagerSQLite oms = new OpenManagerSQLite(this, null);
             SQLiteDatabase bd = oms.getWritableDatabase();
 
@@ -66,7 +72,7 @@ public class ImportBD extends AppCompatActivity implements View.OnClickListener 
             params.put("lsRes", "GetData?table=ville");
 
             // Lancement de la tâche asynchrone
-            new GenericAsyncTask(new MyCallback(textViewMessage, oms, bd)).execute(params);
+            new GenericAsyncTask(new MyCallback(textViewMessage, oms, bd, barreDeProgression)).execute(params);
         }
     }
 
@@ -77,7 +83,7 @@ public class ImportBD extends AppCompatActivity implements View.OnClickListener 
         private TextView textViewMessage;
         private OpenManagerSQLite oms;
         private SQLiteDatabase bd;
-
+        private ProgressBar barreDeProgression;
         /**
          * Constructor
          *
@@ -89,6 +95,20 @@ public class ImportBD extends AppCompatActivity implements View.OnClickListener 
             this.textViewMessage = textViewMessage;
             this.oms = oms;
             this.bd = bd;
+        }
+
+        /**
+         * Constructor
+         * @param textViewMessage
+         * @param oms
+         * @param bd
+         * @param barreDeProgression
+         */
+        public MyCallback(TextView textViewMessage, OpenManagerSQLite oms, SQLiteDatabase bd, ProgressBar barreDeProgression) {
+            this.textViewMessage = textViewMessage;
+            this.oms = oms;
+            this.bd = bd;
+            this.barreDeProgression = barreDeProgression;
         }
 
         /**
@@ -115,6 +135,7 @@ public class ImportBD extends AppCompatActivity implements View.OnClickListener 
          * @param json
          */
         private void saveDataIntoDB(JSONArray json) {
+            int ttJson = json.length();
             try {
                 // début de la transaction
                 bd.beginTransaction();
@@ -131,17 +152,25 @@ public class ImportBD extends AppCompatActivity implements View.OnClickListener 
 
                     // Inserte renvoi le nouvel ID
                     long llNum = bd.insert("ville", null, hmValues);
+
+                    progressBar(ttJson, (i + 1));
                 }
                 // commit
                 bd.setTransactionSuccessful();
                 // Fin de la transaction
                 bd.endTransaction();
                 // Afficher le message de fin
-                this.textViewMessage.setText("Finished");
+//                this.textViewMessage.setText("Finished");
 
             } catch (JSONException e) {
                 e.printStackTrace();
             }
+        }
+
+        private void progressBar(int sizeJson, int nbIsert) {
+            int percent = (100 * nbIsert) / sizeJson;
+            barreDeProgression.setProgress(percent);
+            this.textViewMessage.setText(percent + "%");
         }
     }
 }
---
+++ app/src/main/java/utilities/provider/ProviderSQLiteVille.java
@@ -0,0 +1,69 @@
+package utilities.provider;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+
+import utilities.db.OpenManagerSQLite;
+
+/**
+ * Created by formation on 07/11/2017.
+ */
+public class ProviderSQLiteVille extends ContentProvider {
+
+    private SQLiteDatabase db;
+
+    public static final Uri CONTENT_URI = Uri.parse("content://utilities.provider.ProviderSQLiteVille");
+
+    @Override
+    public boolean onCreate() {
+        Context context = getContext();
+        // Connexion à la BD
+        OpenManagerSQLite dbHelper = new OpenManagerSQLite(context, null);
+        this.db = dbHelper.getWritableDatabase();
+        return (this.db == null) ? false : true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        Cursor curseur = null;
+        // --- Tous les enregistrements
+        try {
+            // --- query(table, tColonnes, sWhere, tParamsWhere, sGroupBy, sHaving, sOrderBy)
+            curseur = this.db.query("ville", projection, selection, selectionArgs, null, null, sortOrder);
+        }
+        catch(SQLiteException e) {
+            curseur = null;
+        }
+        return curseur;
+//        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        return 0;
+    }
+} /// Class
