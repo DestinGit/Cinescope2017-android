2c15ab59f32517442f7e359e5e60801a52918cc5
Destin <bak1tino@gmail.com>
2017-11-14 16:47:17 +0100

    lecture video

--- app/src/main/java/db/fr/cinescope2017/UnFilmUneVideo.java
+++ app/src/main/java/db/fr/cinescope2017/UnFilmUneVideo.java
@@ -5,6 +5,7 @@ import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.MediaController;
 import android.widget.VideoView;
 
 public class UnFilmUneVideo extends AppCompatActivity {
@@ -23,7 +24,9 @@ public class UnFilmUneVideo extends AppCompatActivity {
         // --- Ressource distante ... trop lent
         try {
 //             videoViewVideo.setVideoURI(Uri.parse("http://10.3.50.124/videos/requin.mp4"));
-            videoViewVideo.setVideoURI(Uri.parse("http://www.youtube.com/watch?feature=player_detailpage&v=4aQHyxeo-pg"));
+            videoViewVideo.setMediaController(new MediaController(this));
+//            videoViewVideo.setVideoURI(Uri.parse("http://www.youtube.com/watch?feature=player_detailpage&v=4aQHyxeo-pg"));
+            videoViewVideo.setVideoURI(Uri.parse("http://172.26.10.39/images/tout_l_argent_du_monde.3gpp"));
             videoViewVideo.start();
         } catch (Exception e) {
 
