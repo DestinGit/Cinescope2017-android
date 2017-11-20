6e630ac77ee196db16ae0ed12035074b89c408f3
Destin <bak1tino@gmail.com>
2017-11-20 10:14:07 +0100

    Inclusion dans les layout

---
+++ app/src/main/java/db/fr/cinescope2017/InclusionsDeBase.java
@@ -0,0 +1,13 @@
+package db.fr.cinescope2017;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InclusionsDeBase extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inclusions_de_base);
+    }
+}
